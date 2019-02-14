055aeaf4e9ad5e16fed4be75d258be4076b4b6ac
nanotubing <nanotubing@users.noreply.github.com>
2019-02-14 13:51:07 -0500

    tweaks to install script

---
+++ rstudio_demo.R
@@ -0,0 +1,7 @@
+#install SW
+#recommended GH install didnt work bc proxy
+#manually installed .zip from cran
+devtools::install_github("rstudio/keras")
+library(keras)
+#install_keras(method = "conda")
+install_keras()
