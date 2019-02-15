f782fd6e60b6061334939c373af1f9ceb386d70f
Claude M. Schrader <nanotubing@users.noreply.github.com>
2019-02-15 14:15:47 -0500

    adding python airplane demo

--- rstudio_demo.R
+++ rstudio_demo.R
@@ -1,7 +1,9 @@
 #install SW
 #recommended GH install didnt work bc proxy
 #manually installed .zip from cran
+install.packages("devtools")
 devtools::install_github("rstudio/keras")
 library(keras)
 #install_keras(method = "conda")
 install_keras()
+
