fa4ef14fefc6e89a964102d70261ae524bd3e6b7
Claude M. Schrader <nanotubing@users.noreply.github.com>
2019-04-08 17:16:25 -0400

    cropping mask to image

--- create_perimage_mask.R
+++ create_perimage_mask.R
@@ -5,16 +5,15 @@ install.packages("rgdal")
 library(raster, rgdal)
 
 big_mask = raster("output/maskclass_all.tif")
-big_mask_18n = projectRaster(big_mask, crs = "+proj=utm +zone=18 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+# big_mask_18n = projectRaster(big_mask, crs = "+proj=utm +zone=18 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
 img_dir = "data/planet_training/img"
-images = dir()
+# images = dir()
 mask_dir = "data/planet_training/mask"
 
 filenames = list.files(img_dir, pattern = "*.tif")
 for (f in filenames){
   f_base = tools::file_path_sans_ext(f)
   r = raster(file.path(img_dir, f))
-  crop*=()
-  
+  mask_cropped = crop(big_mask, r)
 }
