f28f1277a98e76893debf57c92ea04dcaccfd202
Claude M. Schrader <nanotubing@users.noreply.github.com>
2019-04-08 14:36:02 -0400

    adding smol mask script

---
+++ create_perimage_mask.R
@@ -0,0 +1,15 @@
+remove(list=ls()) 
+setwd("~/GitHub/keras_playground")
+install.packages("raster")
+install.packages("rgdal")
+library(raster, rgdal)
+
+big_mask = raster("output/maskclass_all.tif")
+img_dir = "data/planet_training/img"
+images = dir()
+mask_dir = "data/planet_training/mask"
+
+filenames = list.files(img_dir, pattern = "*.tif")
+for (f in filenames){
+  f_base = tools::file_path_sans_ext(f)
+}
