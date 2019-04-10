374ccf950da0cc204b2f9d73665fbc18c7c9ef63
Claude M. Schrader <nanotubing@users.noreply.github.com>
2019-04-10 13:00:29 -0400

    bad data script cleanup

--- find_bad_data.py
+++ find_bad_data.py
@@ -5,17 +5,9 @@ Created on Wed Apr 10 11:15:30 2019
 @author: tuj53509
 """
 
-#from unet_model import *
-#from gen_patches import *
 import os, sys, glob
 import numpy as np
 import tifffile as tiff
-#from keras.callbacks import CSVLogger
-#from keras.callbacks import TensorBoard
-#from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-#from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix
-#import matplotlib
-#import matplotlib.pyplot as plt
 
 def normalize(img):
     min = img.min()
@@ -23,15 +15,9 @@ def normalize(img):
     x = 2.0 * (img - min) / (max - min) - 1.0
     return x
 
-
-#image_path = './data/mband/{}.tif'
-#mask_path = './data/gt_mband/{}.tif'
 image_path = './data/planet_training/img/'
 mask_path = './data/planet_training/mask/'
 
-
-#trainIds = [str(i).zfill(2) for i in range(1, 25)]  # all availiable ids: from "01" to "24"
-#trainIds = [ os.path.splitext(file)[0] for file in os.listdir(image_path)]
 trainIds = [os.path.splitext(os.path.basename(file))[0] for file in glob.glob('{}/*_MAT*.tif'.format(image_path))]
 
 if __name__ == '__main__':
@@ -48,5 +34,3 @@ if __name__ == '__main__':
 #        print("Nan values for {}: {}".format(mask, mask_nan))
         if len(np.isnan(mask)) > 0:
             print("Nan values found in {}".format(mask_id))
-    
-  
