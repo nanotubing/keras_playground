4ad4a2de2f4eca064e578ddb0c1fb53cb71e1a5a
Claude M. Schrader <nanotubing@users.noreply.github.com>
2019-05-07 12:56:04 -0400

    go from 4 to 8 channels

--- tools/trim_raster.R
+++ tools/trim_raster.R
@@ -14,4 +14,19 @@ plot(to_match)
 r3 = crop(to_mask, to_match)
 plot(r3)
 
-writeRaster(r3, "data/20180412_143955_0e20_1B_AnalyticMS_crop.tif")
+# r4 = unstack(r3)
+
+list2env(setNames(unstack(r3), names(r3)), .GlobalEnv)
+
+r4 <- stack()
+
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.1)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.2)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.3)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.4)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.1)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.2)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.3)
+r4 <- stack(r4, X20180412_143955_0e20_1B_AnalyticMS.4)
+
+writeRaster(r4, "data/20180412_143955_0e20_1B_AnalyticMS_crop.tif")
